cb3629b022a8447a32980cc2a066ed196174690e
ZzzHhhPpp <13715777132m@sina.cn>
2017-10-22 02:50:11 +0800

    10月22代码

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/Setup2Activity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/Setup2Activity.java
@@ -1,19 +1,48 @@
 package cn.edu.gdmec.android.mobileguard.m2theftguard;
 
+import android.content.SharedPreferences;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telecom.TelecomManager;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 import cn.edu.gdmec.android.mobileguard.R;
 
-public class Setup2Activity extends BaseSetupActivity {
+public class Setup2Activity extends BaseSetupActivity implements View.OnClickListener {
+    private TelephonyManager mTelephonyManager;
+    private Button mBindSIMBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup2);
-
+        //设置第二个小圆点的颜色
         ((RadioButton)findViewById(R.id.rb_second)).setChecked(true);
+        //获取电话管理器这个服务
+        mTelephonyManager = (TelephonyManager)getSystemService(TELEPHONY_SERVICE);
+        //找到布局中的SIM卡绑定
+        mBindSIMBtn=(Button)findViewById(R.id.btn_bind_sim) ;
+        mBindSIMBtn.setOnClickListener(this);
+        if (isBind()){
+            mBindSIMBtn.setEnabled(false);
+        }else{
+            mBindSIMBtn.setEnabled(true);
+        }
+
+    }
+    private boolean isBind(){
+        //sp是父类baseSetupActivity的属性，是sharedPreference, 按ctrl+鼠标左键就能 跳转到声明的位置
+        String simString =sp.getString("sim",null);
+        if (TextUtils.isEmpty(simString)){
+            return false;
+        }
+        return true;
     }
     @Override
     public void showNext(){
@@ -22,5 +51,35 @@ public class Setup2Activity extends BaseSetupActivity {
     @Override
     public void showPre(){
         startActivityAndFinishSelf(Setup1Activity.class);
+    }
+    @Override
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.btn_bind_sim:
+                //绑定sim卡
+                bindSIM();
+                break;
+        }
+    }
+
+    /**
+     * *
+     * 绑定sim卡
+     */
+    private void bindSIM(){
+        if (!isBind()){
+            //使用电话管理器服务来获取sim卡号
+            String simSerialNumber = mTelephonyManager.getSimSerialNumber();
+            //存储sim卡号
+            SharedPreferences.Editor edit = sp.edit();
+            edit.putString("sim",simSerialNumber);
+            edit.commit();
+            Toast.makeText(this,"SIM卡绑定成功！",Toast.LENGTH_LONG).show();
+            mBindSIMBtn.setEnabled(false);
+        }else{
+            //已经绑定，提醒用户
+            Toast.makeText(this,"SIM卡已经绑定",Toast.LENGTH_LONG).show();
+            mBindSIMBtn.setEnabled(false);
+        }
     }
 }
