4dacb048c3e593f8b9bdd7889bc3e658abd5b7ad
Wuwu2017 <740021228@qq.com>
2017-11-13 17:10:18 +0800

    11.13

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m5virusscan/VirusScanSpeedActivity.java
@@ -0,0 +1,10 @@
+package cn.edu.gdmec.android.mobileguard.m5virusscan;
+
+import android.view.View;
+
+/**
+ * Created by HP on 2017/11/13.
+ */
+
+public class VirusScanSpeedActivity implements View.OnClickListener {
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m5virusscan/entity/ScanAppInfo.java
@@ -0,0 +1,15 @@
+package cn.edu.gdmec.android.mobileguard.m5virusscan.entity;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by HP on 2017/11/13.
+ */
+
+public class ScanAppInfo {
+    public String appName;
+    public String packagename;
+    public boolean isVirus;
+    public String description;
+    public Drawable appicon;
+}
